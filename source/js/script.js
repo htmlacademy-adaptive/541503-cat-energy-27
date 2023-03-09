667c12af0dacfb782a24e12d90ddc1d51ca21d51
Oleg Tsyganov <redsmoke19@gmail.com>
2023-03-09 12:40:41 +0400

    fix editorconfig

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,11 @@
-﻿const mainNav = document.querySelector('.page-header__main-nav');
-
+const mainNav = document.querySelector('.page-header__main-nav');
 mainNav.classList.remove('page-header__main-nav--opened');
-
 const pageHeaderToggle = document.querySelector('.page-header__toggle');
 
-pageHeaderToggle.onclick = function () {
+pageHeaderToggle.addEventListener('click', function() {
   if (mainNav.classList.contains('page-header__main-nav--opened')) {
     mainNav.classList.remove('page-header__main-nav--opened');
   } else {
     mainNav.classList.add('page-header__main-nav--opened');
   }
-};
+});
